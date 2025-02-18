9eae70a374b3e68f1bd6590b56358389f247cb08
kimil61 <illllllkim61@gmail.com>
2025-02-18 12:24:23 +0800

    비행기의 x,z축 기준으로의 움직임을 실현. 단 x축 기준 -25,25 z축 기준 -1,40 사이가 최대값.

--- Assets/Flight3D/Scripts/PlayerController.cs
+++ Assets/Flight3D/Scripts/PlayerController.cs
@@ -4,10 +4,21 @@ namespace Flight3D
 {
     public class PlayerController : MonoBehaviour
     {
+        public float speed = 20.0f;
+        public float rotationSpeed = 100.0f;
 
         void Update()
         {
-           
+            float _x = Input.GetAxis("Horizontal");
+            float _z = Input.GetAxis("Vertical");
+
+            Vector3 newPosition = transform.position + new Vector3(_x, 0, _z) * (speed * Time.deltaTime);
+            
+            newPosition.x = Mathf.Clamp(newPosition.x,-25,25);
+            newPosition.z = Mathf.Clamp(newPosition.z,-1,40);
+            
+            transform.position = newPosition;
+
 
 
         }
